1deb933a023ec61ba6e71b2fc83cb553f80209af
Yuxi Wu <yuxiwu@uchicago.edu>
2018-01-29 19:46:56 -0600

    reorganised functions

---
+++ lab1/cleaner.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+import json
+import peakutils
+import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC
+from sklearn.grid_search import GridSearchCV
+from sklearn.learning_curve import validation_curve
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+import seaborn as sns
+
+def load_json(file):
+    '''
+    Loads txt file and returns json dict
+    '''
+    data = {}
+    with open(file, encoding='utf-8') as f:
+        for line in f:
+            d = str(line).replace("'", '"')
+            data = json.loads(d)
+
+    return data
+
+def get_sequence(data_list, sensor):
+    '''
+    Return list of sensor data from data_list
+    '''
+    l = []
+    for i in range(len(data_list)):
+        lst = []
+        for j in range(len(data_list[i]['seq'])):
+            lst.append(data_list[i]['seq'][j]['data'][sensor])
+        l.append(lst)
+
+    return l
+
+def get_mean_sd_peaks(seq_data):
+    '''
+    Calculates the mean, sd, and number of peaks for each sequence of data for each sensor
+    '''
+    means = []
+    stds = []
+    peaks = []
+    for lst in seq_data:
+        means.append(np.mean(lst))
+        stds.append(np.std(lst))
+        peaks.append(len(peakutils.indexes(lst, thres=0.02/max(lst), min_dist=0.1)))
+
+    return means, stds, peaks
+
+def process_sequence(file):
+    '''
+    Loads data and creates dictionary of calculations from accelerometer data
+    '''
+    data = load_json(file)
+
+    # obtain actitivy from trace
+    activity = data[0]['type']
+
+    # obtain accelerometer data from each axis
+    x_accl = get_sequence(data, 'xAccl')
+    y_accl = get_sequence(data, 'yAccl')
+    z_accl = get_sequence(data, 'zAccl')
+
+    # obtain gyroscope data from each axis
+    x_gyro = get_sequence(data, 'xGyro')
+    y_gyro = get_sequence(data, 'yGyro')
+    z_gyro = get_sequence(data, 'zGyro')
+
+    # obtain mag sensor data from each axis
+    x_mag = get_sequence(data, 'xMag')
+    y_mag = get_sequence(data, 'yMag')
+    z_mag = get_sequence(data, 'zMag')
+
+    # calculate means, sd, num peaks for data sequence
+    x_accl_mean, x_accl_sd, x_accl_peaks = get_mean_sd_peaks(x_accl)
+    y_accl_mean, y_accl_sd, y_accl_peaks = get_mean_sd_peaks(y_accl)
+    z_accl_mean, z_accl_sd, z_accl_peaks = get_mean_sd_peaks(z_accl)
+
+    x_gyro_mean, x_gyro_sd, x_gyro_peaks = get_mean_sd_peaks(x_gyro)
+    y_gyro_mean, y_gyro_sd, y_gyro_peaks = get_mean_sd_peaks(y_gyro)
+    z_gyro_mean, z_gyro_sd, z_gyro_peaks = get_mean_sd_peaks(z_gyro)
+
+    x_mag_mean, x_mag_sd, x_mag_peaks = get_mean_sd_peaks(x_mag)
+    y_mag_mean, y_mag_sd, y_mag_peaks = get_mean_sd_peaks(y_mag)
+    z_mag_mean, z_mag_sd, z_mag_peaks = get_mean_sd_peaks(z_mag)
+
+    clean_data = {
+        'activity': [],
+        'x_accl_mean': [], 'x_accl_sd': [], 'x_accl_peaks': [],
+        'y_accl_mean': [], 'y_accl_sd': [], 'y_accl_peaks': [],
+        'z_accl_mean': [], 'z_accl_sd': [], 'z_accl_peaks': [],
+        'x_gyro_mean': [], 'x_gyro_sd': [], 'x_gyro_peaks': [],
+        'y_gyro_mean': [], 'y_gyro_sd': [], 'y_gyro_peaks': [],
+        'z_gyro_mean': [], 'z_gyro_sd': [], 'z_gyro_peaks': [],
+        'x_mag_mean': [], 'x_mag_sd': [], 'x_mag_peaks': [],
+        'y_mag_mean': [], 'y_mag_sd': [], 'y_mag_peaks': [],
+        'z_mag_mean': [], 'z_mag_sd': [], 'z_mag_peaks': []
+    }
+
+    for i in range(len(x_accl_mean)):
+        clean_data['activity'].append(activity)
+        clean_data['x_accl_mean'].append(x_accl_mean[i])
+        clean_data['x_accl_sd'].append(x_accl_sd[i])
+        clean_data['x_accl_peaks'].append(x_accl_peaks[i])
+        clean_data['y_accl_mean'].append(y_accl_mean[i])
+        clean_data['y_accl_sd'].append(y_accl_sd[i])
+        clean_data['y_accl_peaks'].append(y_accl_peaks[i])
+        clean_data['z_accl_mean'].append(z_accl_mean[i])
+        clean_data['z_accl_sd'].append(z_accl_sd[i])
+        clean_data['z_accl_peaks'].append(z_accl_peaks[i])
+        clean_data['x_gyro_mean'].append(x_gyro_mean[i])
+        clean_data['x_gyro_sd'].append(x_gyro_sd[i])
+        clean_data['x_gyro_peaks'].append(x_gyro_peaks[i])
+        clean_data['y_gyro_mean'].append(y_gyro_mean[i])
+        clean_data['y_gyro_sd'].append(y_gyro_sd[i])
+        clean_data['y_gyro_peaks'].append(y_gyro_peaks[i])
+        clean_data['z_gyro_mean'].append(z_gyro_mean[i])
+        clean_data['z_gyro_sd'].append(z_gyro_sd[i])
+        clean_data['z_gyro_peaks'].append(z_gyro_peaks[i])
+        clean_data['x_mag_mean'].append(x_mag_mean[i])
+        clean_data['x_mag_sd'].append(x_mag_sd[i])
+        clean_data['x_mag_peaks'].append(x_mag_peaks[i])
+        clean_data['y_mag_mean'].append(y_mag_mean[i])
+        clean_data['y_mag_sd'].append(y_mag_sd[i])
+        clean_data['y_mag_peaks'].append(y_mag_peaks[i])
+        clean_data['z_mag_mean'].append(z_mag_mean[i])
+        clean_data['z_mag_sd'].append(z_mag_sd[i])
+        clean_data['z_mag_peaks'].append(z_mag_peaks[i])
+
+    df = pd.DataFrame(clean_data)
+    return df
+
+def plot_traces(plots, activity):
+    x,y,z = plots
+    plt.figure(figsize=(15,5))
+    plt.plot(x, label='x')
+    plt.plot(y, label='y')
+    plt.plot(z, label='z')
+    plt.legend()
+    plt.title(activity)
+    plt.savefig(activity + '.png')
+    plt.show()
+
+def master():
+    master = []
+    for i in ['driving','standing','walking','jumping']:
+        df = process_sequence('class-data/' + i + '.txt')
+        master += [df]
+    master = pd.concat(master)
+    master['activity_factor'] = pd.factorize(master['activity'])[0]
+    return master
+
+def splits(df):
+    y = df['activity_factor']
+    X = df.drop(['activity_factor', 'activity'], axis=1)
+    return train_test_split(X, y, test_size=0.25, random_state=1234)
---
+++ lab1/loop.py
@@ -0,0 +1,178 @@
+import pandas as pd
+import numpy as np
+import re
+import sys
+import time
+from sklearn import preprocessing, cross_validation, svm, metrics, tree, decomposition, svm
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier, OrthogonalMatchingPursuit, RandomizedLogisticRegression
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import ParameterGrid
+from sklearn.metrics import *
+from sklearn.preprocessing import RobustScaler, StandardScaler
+
+
+def define_clfs_params(grid_size):
+
+    clfs = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
+        'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
+        'AB': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200),
+        'LR': LogisticRegression(penalty='l1', C=1e5),
+        'SVM': svm.SVC(kernel='linear', probability=True, random_state=0),
+        'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=10),
+        'DT': DecisionTreeClassifier(),
+        'KNN': KNeighborsClassifier(n_neighbors=3)
+            }
+
+    large_grid = {
+    'RF':{'n_estimators': [1,10,100,1000,10000], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
+    # 'ET': { 'n_estimators': [1,10,100,1000,10000], 'criterion' : ['gini', 'entropy'] ,'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+    # 'GB': {'n_estimators': [1,10,100,1000,10000], 'learning_rate' : [0.001,0.01,0.05,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
+    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+    # 'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+           }
+
+    small_grid = {
+    'RF':{'n_estimators': [1,10,100], 'max_depth': [1,5,10,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.001,0.1,1,10]},
+    # 'ET': { 'n_estimators': [10,100], 'criterion' : ['gini', 'entropy'] ,'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+    # 'GB': {'n_estimators': [10,100], 'learning_rate' : [0.001,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [5,50]},
+    'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10]},
+    'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+    # 'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+           }
+
+    test_grid = {
+    'RF':{'n_estimators': [1], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'LR': { 'penalty': ['l1'], 'C': [0.01]},
+    # 'ET': { 'n_estimators': [1], 'criterion' : ['gini'] ,'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'AB': { 'algorithm': ['SAMME'], 'n_estimators': [1]},
+    # 'GB': {'n_estimators': [1], 'learning_rate' : [0.1],'subsample' : [0.5], 'max_depth': [1]},
+    'DT': {'criterion': ['gini'], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+    'SVM' :{'C' :[0.01],'kernel':['linear']},
+    # 'KNN' :{'n_neighbors': [5],'weights': ['uniform'],'algorithm': ['auto']}
+           }
+
+    if (grid_size == 'large'):
+        return clfs, large_grid
+    elif (grid_size == 'small'):
+        return clfs, small_grid
+    elif (grid_size == 'test'):
+        return clfs, test_grid
+    else:
+        return 0, 0
+
+
+def generate_binary_at_k(y_scores, k):
+    cutoff_index = int(len(y_scores) * (k / 100.0))
+    test_predictions_binary = [1 if x < cutoff_index else 0 for x in range(len(y_scores))]
+    return test_predictions_binary
+
+def recall_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    return recall_score(y_true, preds_at_k, average='binary')
+
+def precision_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    precision = precision_score(y_true, preds_at_k)
+    return precision
+
+def accuracy_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    return accuracy_score(y_true, preds_at_k)
+
+def f1_at_k(y_true, y_scores, k):
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    return f1_score(y_true, preds_at_k, average='binary')
+
+def get_feature_importance(clf, model_name):
+    clfs = {'RF':'feature_importances',
+            'LR': 'coef',
+            'SVM': 'coef',
+            'DT': 'feature_importances',
+            # 'KNN': None,
+            'AB': 'feature_importances',
+            # 'GB': None,
+            'linear.SVC': 'coef',
+            # 'ET': 'feature_importances'
+            }
+
+    if clfs[model_name] == 'feature_importances':
+        return  list(clf.feature_importances_)
+    elif clfs[model_name] == 'coef':
+        return  list(clf.coef_.tolist())
+    else:
+        return None
+
+
+def clf_loop(models_to_run, clfs, grid, X_train, y_train, X_test, y_test):
+    results_df =  pd.DataFrame(columns=('model_type','clf', 'parameters', 'time_used',
+                                        'auc-roc','accuracy',
+                                        'p_at_5','p_at_10', 'p_at_30', 'p_at_50',
+                                        'r_at_5','r_at_10','r_at_30', 'r_at_50',
+                                        'a_at_5','a_at_10','a_at_30', 'a_at_50',
+                                        'f1_at_5','f1_at_10','f1_at_30', 'f1_at_50',
+                                        'feature_importance', 'col_used_for_feat_importance'))
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+    for index, clf in enumerate([clfs[x] for x in models_to_run]):
+        print(models_to_run[index])
+        parameter_values = grid[models_to_run[index]]
+        for p in ParameterGrid(parameter_values):
+            try:
+                clf.set_params(**p)
+                start_time = time.time()
+                clf.fit(X_train, y_train)
+                y_pred = clf.predict(X_test)
+                y_pred_probs = list(clf.predict_proba(X_test)[:,1])
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                y_pred_probs_sorted, y_test_sorted = zip(*sorted(zip(y_pred_probs, y_test), reverse=True))
+                results_df.loc[len(results_df)] = [models_to_run[index],clf, p, elapsed_time,
+                                                   roc_auc_score(y_test, y_pred_probs),
+                                                   accuracy_score(y_test,y_pred),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,30.0),
+                                                   precision_at_k(y_test_sorted,y_pred_probs_sorted,50.0),
+                                                   recall_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                   recall_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                   recall_at_k(y_test_sorted,y_pred_probs_sorted,30.0),
+                                                   recall_at_k(y_test_sorted,y_pred_probs_sorted,50.0),
+                                                   accuracy_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                   accuracy_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                   accuracy_at_k(y_test_sorted,y_pred_probs_sorted,30.0),
+                                                   accuracy_at_k(y_test_sorted,y_pred_probs_sorted,50.0),
+                                                   f1_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                                   f1_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                                   f1_at_k(y_test_sorted,y_pred_probs_sorted,30.0),
+                                                   f1_at_k(y_test_sorted,y_pred_probs_sorted,50.0),
+                                                   get_feature_importance(clf, models_to_run[index]),
+                                                   list(X_train.columns.values)]
+
+            except IndexError:
+                print("IndexError")
+                continue
+    return results_df
+
+
+def go(df_train, df_test, label='withdraw_reason', models=['LR','DT','RF','AB', 'SVM'], grid_size='test', result_file='report.pkl'):
+    clfs, grid = define_clfs_params(grid_size)
+    train_features = [x for x in df_train.columns if x!=label]
+    test_features = [x for x in df_test.columns if x!=label]
+    X_train = df_train[train_features]
+    y_train = df_train[label]
+    X_test = df_test[test_features]
+    y_test = df_test[label]
+    results_df = clf_loop(models, clfs, grid, X_train, y_train, X_test, y_test)
+    # manually change this to match the model you run
+    results_df.to_pickle(result_file)
+    return results_df
